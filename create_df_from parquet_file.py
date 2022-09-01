b8669c60b71431542d9c0a5cfd8bcef3edc00868
ergemp <ergemp@gmail.com>
2022-09-01 17:13:39 +0300

    sql function examples added

--- create_df_from parquet_file.py
+++ create_df_from parquet_file.py
@@ -7,11 +7,11 @@ df.write.parquet("output/people.parquet")
 parDF1 = spark.read.parquet("data/people.parquet")
 '''
 
-data = [("James ","","Smith","36636","M",3000),
-        ("Michael ","Rose","","40288","M",4000),
-        ("Robert ","","Williams","42114","M",4000),
-        ("Maria ","Anne","Jones","39192","F",4000),
-        ("Jen","Mary","Brown","","F",-1)]
+data = [("James ", "", "Smith", "36636", "M", 3000),
+        ("Michael ", "Rose", "", "40288", "M", 4000),
+        ("Robert ", "", "Williams", "42114", "M", 4000),
+        ("Maria ", "Anne", "Jones", "39192", "F", 4000),
+        ("Jen", "Mary", "Brown", "", "F", -1)]
 
 columns = ["firstname", "middlename", "lastname", "dob", "gender", "salary"]
 
